0789a2649318f2a43705309657b4d8d19cfdc338
MidnightShad0w <samsonchik2406@mail.ru>
2025-02-12 15:51:38 +0300

    UploadService testing, first tries

--- UploadService/src/main/java/com/danila/uploadservice/service/KafkaProducerService.java
+++ UploadService/src/main/java/com/danila/uploadservice/service/KafkaProducerService.java
@@ -1,7 +1,6 @@
 package com.danila.uploadservice.service;
 
 import com.danila.uploadservice.model.FileUploadMessage;
-import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
