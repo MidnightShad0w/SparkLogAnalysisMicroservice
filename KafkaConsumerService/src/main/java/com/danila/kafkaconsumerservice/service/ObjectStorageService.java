8c06ce93b1531e43a3c272d96b9309369ba4bc01
MidnightShad0w <samsonchik2406@mail.ru>
2025-02-18 16:51:27 +0300

    add file uploading to cloud.ru object storage

---
+++ KafkaConsumerService/src/main/java/com/danila/kafkaconsumerservice/service/ObjectStorageService.java
@@ -0,0 +1,47 @@
+package com.danila.kafkaconsumerservice.service;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+public class ObjectStorageService {
+
+    private final AmazonS3 s3Client;
+    private final String bucketName;
+
+    public ObjectStorageService(
+            @Value("${cloudru.tenant-id}") String tenantId,
+            @Value("${cloudru.key-id}") String keyId,
+            @Value("${cloudru.key-secret}") String keySecret,
+            @Value("${cloudru.endpoint}") String endpoint,
+            @Value("${cloudru.region}") String region,
+            @Value("${cloudru.bucket-name}") String bucketName) {
+
+        this.bucketName = bucketName;
+        // Формируем aws_access_key_id в виде tenantId:keyId
+        String accessKey = tenantId + ":" + keyId;
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, keySecret);
+        ClientConfiguration clientConfig = new ClientConfiguration();
+        // Рекомендуется принудительно использовать подпись V4
+        clientConfig.setSignerOverride("AWSS3V4SignerType");
+
+        this.s3Client = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .withPathStyleAccessEnabled(true) // Обязательно для многих S3-совместимых сервисов
+                .withClientConfiguration(clientConfig)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+    public void uploadFile(String key, File file) {
+        s3Client.putObject(bucketName, key, file);
+    }
+}
