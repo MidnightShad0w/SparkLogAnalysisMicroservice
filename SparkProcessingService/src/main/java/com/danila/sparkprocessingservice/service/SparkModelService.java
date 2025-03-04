93bbd0593c77aafcdf256c41faadae5a6697ab59
MidnightShad0w <samsonchik2406@mail.ru>
2025-03-04 15:45:43 +0300

    airflow works good, model processing files, deleting files after process, all results in folder saved

--- SparkProcessingService/src/main/java/com/danila/sparkprocessingservice/service/SparkModelService.java
+++ SparkProcessingService/src/main/java/com/danila/sparkprocessingservice/service/SparkModelService.java
@@ -1,5 +1,14 @@
 package com.danila.sparkprocessingservice.service;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.apache.spark.ml.PipelineModel;
 
 import org.apache.spark.ml.clustering.KMeansModel;
@@ -14,6 +23,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 import static org.apache.spark.sql.functions.callUDF;
 import static org.apache.spark.sql.functions.col;
 
@@ -22,14 +33,19 @@ public class SparkModelService {
 
     private final SparkSession spark;
 
-//    @Value("${minio.endpoint}")
-//    private String minioEndpoint;
-//
-//    @Value("${minio.accessKey}")
-//    private String minioAccessKey;
-//
-//    @Value("${minio.secretKey}")
-//    private String minioSecretKey;
+    @Value("${minio.bucket}")
+    private String bucketName;
+
+    @Value("${minio.endpoint}")
+    private String minioEndpoint;
+
+    @Value("${minio.accessKey}")
+    private String minioAccessKey;
+
+    @Value("${minio.secretKey}")
+    private String minioSecretKey;
+
+    private AmazonS3 s3Client;
 
     @Autowired
     public SparkModelService() {
@@ -43,6 +59,24 @@ public class SparkModelService {
                 .config("spark.hadoop.fs.s3a.path.style.access", "true")
                 .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
                 .getOrCreate();
+    }
+
+    @PostConstruct
+    public void initS3Client() {
+        // Создаем учетные данные
+        BasicAWSCredentials credentials = new BasicAWSCredentials(minioAccessKey, minioSecretKey);
+        // Создаем конфигурацию клиента
+        ClientConfiguration clientConfig = new ClientConfiguration();
+        // Указываем использовать S3V4 подписывание
+        clientConfig.setSignerOverride("AWSS3V4SignerType");
+
+        // Инициализируем s3Client с настройками для MinIO
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(minioEndpoint, "us-east-1"))
+                .withPathStyleAccessEnabled(true)
+                .withClientConfiguration(clientConfig)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
     }
 
     public void processFile(String csvPath, String modelPath, String resultPath) {
@@ -89,6 +123,27 @@ public class SparkModelService {
                 .parquet(resultPath);
 
         System.out.println("Результаты сохранены в: " + resultPath);
+
+        deleteProcessedFiles();
+    }
+
+    public void deleteProcessedFiles() {
+        try {
+            ListObjectsV2Request req = new ListObjectsV2Request()
+                    .withBucketName(bucketName)
+                    .withPrefix("uploads/");
+            ListObjectsV2Result result;
+            do {
+                result = s3Client.listObjectsV2(req);
+                for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+                    s3Client.deleteObject(bucketName, objectSummary.getKey());
+                    System.out.println("Deleted: " + objectSummary.getKey());
+                }
+                req.setContinuationToken(result.getNextContinuationToken());
+            } while (result.isTruncated());
+        } catch (Exception e) {
+            System.err.println("Ошибка при удалении файлов из папки uploads: " + e.getMessage());
+        }
     }
 }
 
